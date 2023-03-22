d9fd1a82092b454e68b5716118681d8feab6b4c1
kemboy-svg <eliaskemboy645@gmail.com>
2023-03-22 12:17:28 +0300

    changes

--- mscbot/static/scripts/responses.js
+++ mscbot/static/scripts/responses.js
@@ -1,21 +1,62 @@
 function getBotResponse(input) {
 
-    if (input == "Hello") {
+    if (input == "Hello", "hello there", "Hi","Good morning") {
+       
         return "Hello there, how can we help?" ;
-    } else if (input == "Good morning") {
+    } 
+    else if (input == "Good morning") {
         return "Good morning, how can I help?";
     } else if (input == "Can I see a physician this weekend?") {
         return "Yes, the physician is available, schedule us your time";
-        
-        
-    }
+       
+        }
+         
+
 
     
-    if (input == "hello") {
-        return "Hello there!";
-    } else if (input == "goodbye") {
+    if (input == "I need help?") {
+        return "I am here, How can I help?";
+        
+    } 
+    else if (input == "I feel my eyes red") {
+        return "schedule a date to meet your doctor";
+    }
+    else if (input == "Are you available during the weekends?") {
+        return "Yes ";
+    }
+    else if (input == "prescribe headache?") {
+        return "panadol,amoxil 2*3";
+    }
+    else if (input == "goodbye") {
         return "Talk to you later!";
     } else {
-        return "Try asking something else!";
+        return "Let us reach you to the nearest doctor";
+    }
+
+
+    if (input == "I have an headache") {
+        return "What are your symptoms?";
+        
+    } 
+    else if (input == "Pain in the head","discomfort in the head","pain in the neck"
+    "A feeling of tightness or pressure in the head",
+    "Throbbing or pulsating pain",
+    "Sensitivity to light or sound",
+    "Nausea ","vomiting",
+    "Dizziness","lightheadedness",
+    "Fatigue","weakness",
+    "Difficulty concentrating",
+    "Difficulty sleeping",
+    "Changes in vision or hearing") {
+        return "Rest:Get some rest and try to relax",
+
+        "Hydration: Drink plenty of water, as dehydration can often cause headaches",
+        
+        "Over-the-counter pain relief: You could try taking an over-the-counter pain medication, such as acetaminophen, ibuprofen, or aspirin. Be sure to follow the recommended dosage and consult your doctor if you have any concerns",
+        
+        "Avoid triggers: Try to identify and avoid triggers that can cause your headaches, such as certain foods, stress, or lack of sleep",
+        
+       "Massage: Gentle massage on the head, neck, and shoulders can sometimes help to relieve headaches",
+        "schedule a date to meet your doctor";
     }
 }
